a0d7a630456331089a314536d77d55ca1f402eeb
SaraMarques <sarasmarques16@gmail.com>
2022-06-29 21:06:43 -0300

    versao inicial

---
+++ script.js
@@ -0,0 +1,32 @@
+const nome              = "Sara"
+const sobrenome         = "marques"
+const data_nascimento   = "22/01/2006"
+
+let altura              = 1.75
+let numero_sapato       = 38
+let idade               = 16
+
+let celular             = "(18)93500-0032"
+let cor_favorita        = "azul"
+
+let qtde_moto           =null
+let qtde_carro          =null
+
+//         <- o valor
+qtde_carro = 1
+
+altura = 1.74
+
+celular = "(18) 99661-2234"
+
+// comparar/validar/verificar Igualdade
+
+nome.trim().toUpperCase() == 
+
+idade == 16 // true
+
+idade = 17
+
+idade == 17 //true
+
+(true == false) == (true == false)
